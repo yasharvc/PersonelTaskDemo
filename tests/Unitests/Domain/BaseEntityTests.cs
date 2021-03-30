b328cf87ceae4e2ccc811fdd08e3306c3c3c1f31
Yashar Aliabbasi <yashar@pragmateam.dev>
2021-03-30 21:54:05 +0430

    Assure that created property is set to now

--- tests/Unitests/Domain/BaseEntityTests.cs
+++ tests/Unitests/Domain/BaseEntityTests.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using DomainLayer.Common;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace Unitests.Domain
 {
 	public class BaseEntityTests
 	{
-		
+		class TestEntity : BaseEntity
+		{
+
+		}
+		[Fact]
+		public void When_Inistanciate_Should_Created_Has_Valid_DateTime()
+		{
+			var beforeCreationDateTime = DateTime.Now;
+			var entity = new TestEntity();
+
+			Assert.True(entity.Created.CompareTo(beforeCreationDateTime) > 0);
+		}
 	}
 }
