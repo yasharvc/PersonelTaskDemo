26fb466875c496bc9cb926adc109bbe4370612cc
Yashar Aliabbasi <yashar@pragmateam.dev>
2021-03-30 22:52:01 +0430

    Tested InMemory ability of EF

---
+++ tests/Unitests/Infrastructure/PersistanceTests.cs
@@ -0,0 +1,34 @@
+﻿using DomainLayer.Entities;
+using InfrastructureLayer.Persistance;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Unitests.Infrastructure
+{
+	public class PersistanceTests
+	{
+		[Fact]
+		public async void With_InMemorySetting_DbContext_Should_Connect_To_InMemory()
+		{
+			var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+			.UseInMemoryDatabase(databaseName: "Test")
+			.Options;
+
+			using var context = new ApplicationDbContext(options);
+			var p = new Personel
+			{
+				FirstName = "Yashar",
+				LastName = "Aliabbasi",
+				DateOfBirth = DateTime.Now.AddYears(-30)
+			};
+
+			context.Personels.Add(p);
+			context.SaveChanges();
+			Assert.Equal(1, await context.Personels.CountAsync());
+
+		}
+	}
+}
