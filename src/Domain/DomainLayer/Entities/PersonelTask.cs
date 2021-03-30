8addd38f77b746946cfdaed5bbdd0c5e85834845
Yashar Aliabbasi <yashar@pragmateam.dev>
2021-03-30 22:33:09 +0430

    Added PersonelTask Entity

---
+++ src/Domain/DomainLayer/Entities/PersonelTask.cs
@@ -0,0 +1,13 @@
+﻿using DomainLayer.Common;
+
+namespace DomainLayer.Entities
+{
+	public class PersonelTask : BaseEntity
+	{
+		public string PersonelId { get; set; }
+		public Personel Personel { get; set; }
+
+		public string TaskId { get; set; }
+		public Task Task { get; set; }
+	}
+}
