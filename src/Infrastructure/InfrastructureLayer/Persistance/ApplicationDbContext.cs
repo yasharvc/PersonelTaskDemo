37630a7b3be224f652dbc7debb6e64432d2030c7
Yashar Aliabbasi <yashar@pragmateam.dev>
2021-03-30 22:44:37 +0430

    Created Application DB Context by EF core

---
+++ src/Infrastructure/InfrastructureLayer/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using DomainLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace InfrastructureLayer.Persistance
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+		{
+
+		}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PersonelTask>().HasKey(pt => new { pt.PersonelId, pt.TaskId });
+
+            modelBuilder.Entity<PersonelTask>()
+                .HasOne(p => p.Personel)
+                .WithMany(s => s.PersonelTasks)
+                .HasForeignKey(sc => sc.PersonelId);
+
+
+            modelBuilder.Entity<PersonelTask>()
+                .HasOne(p => p.Task)
+                .WithMany(s => s.PersonelTasks)
+                .HasForeignKey(sc => sc.TaskId);
+        }
+
+        public DbSet<Personel> Personels { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<PersonelTask> PersonelTasks { get; set; }
+    }
+}
