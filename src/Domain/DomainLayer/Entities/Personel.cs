e178eb3528877f1929ff3f67d6d69b03d7139e58
Yashar Aliabbasi <yashar@pragmateam.dev>
2021-03-30 22:08:50 +0430

    Created Personel Entity

---
+++ src/Domain/DomainLayer/Entities/Personel.cs
@@ -0,0 +1,14 @@
+﻿using DomainLayer.Common;
+using System;
+using System.Collections.Generic;
+
+namespace DomainLayer.Entities
+{
+	public class Personel : BaseEntity
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public DateTime DateOfBirth { get; set; }
+		public virtual ICollection<PersonelAddress> Addresses { get; set; }
+	}
+}
