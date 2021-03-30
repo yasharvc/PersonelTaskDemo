253d90329e54d1a1a87ff5e744e50f556c643f00
Yashar Aliabbasi <yashar@pragmateam.dev>
2021-03-30 22:06:50 +0430

    Added PersonelAddress entity

---
+++ src/Domain/DomainLayer/Entities/PersonelAddress.cs
@@ -0,0 +1,12 @@
+﻿using DomainLayer.Common;
+
+namespace DomainLayer.Entities
+{
+	public class PersonelAddress : BaseEntity
+	{
+		public string Country { get; set; }
+		public string City { get; set; }
+		public string Street { get; set; }
+		public string No { get; set; }
+	}
+}
