5991524bbc76a4e8c2c72505552c41492bd08349
Yashar Aliabbasi <yashar@pragmateam.dev>
2021-03-31 19:52:50 +0430

    Added base class that support DI

---
+++ tests/IntegrationTests/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using ApplicationLayer.Common.Interfaces;
+using InfrastructureLayer.Persistance;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IntegrationTests
+{
+	public abstract class DependencyInjection
+	{
+		IServiceCollection Services { get; } = new ServiceCollection();
+
+		protected ServiceProvider ServiceProvider { get; }
+
+		public DependencyInjection()
+		{
+			Services.AddSingleton<IApplicationDbContext, ApplicationDbContext>();
+			AddAdditionalServices(Services);
+			ServiceProvider = Services.BuildServiceProvider();
+		}
+
+		protected abstract void AddAdditionalServices(IServiceCollection services);
+	}
+}
