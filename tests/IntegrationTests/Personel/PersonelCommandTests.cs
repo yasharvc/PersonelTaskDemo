a01a813d42edd2c2cf3d388e8d9235c550fc2904
Yashar Aliabbasi <yashar@pragmateam.dev>
2021-04-01 01:39:03 +0430

    Tested Create Personel Command

---
+++ tests/IntegrationTests/Personel/PersonelCommandTests.cs
@@ -0,0 +1,38 @@
+﻿using ApplicationLayer.Common.Interfaces;
+using ApplicationLayer.Personel.CreatePersonel;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading;
+using Xunit;
+
+namespace IntegrationTests.Personel
+{
+	public class PersonelCommandTests : DependencyInjection
+	{
+		protected override void AddAdditionalServices(IServiceCollection services)
+		{
+			services.AddSingleton<IRequest<DomainLayer.Entities.Personel>, CreatePersonelCommand>();
+			services.AddSingleton<IRequestHandler<CreatePersonelCommand, DomainLayer.Entities.Personel>, CreatePersonelCommandHandler>();
+		}
+
+		[Fact]
+		public async void Should_Create_Entity_Successfully()
+		{
+			var cmd = new CreatePersonelCommand
+			{
+				FirstName = "Yashar",
+				LastName = "Aliabbasi",
+				DateOfBirth = DateTime.Now.AddYears(-30)
+			};
+
+			var handler = ServiceProvider.GetService<IRequestHandler<CreatePersonelCommand, DomainLayer.Entities.Personel>>();
+
+			var result = await handler.Handle(cmd, new CancellationToken());
+
+			Assert.False(string.IsNullOrEmpty(result.Id));
+			Assert.Equal(1, await ApplicationDbContext.Personels.CountAsync());
+			Assert.Equal(result.Id, (await ApplicationDbContext.Personels.FirstAsync()).Id);
+		}
+	}
+}
