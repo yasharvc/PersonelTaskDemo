4c7f1431d5c31ecda827aaa01491710de510603b
Yashar Aliabbasi <yashar@pragmateam.dev>
2021-04-01 01:12:05 +0430

    Added deleting task command

---
+++ src/Application/ApplicationLayer/Task/Commands/DeleteTask/DeleteTaskCommand.cs
@@ -0,0 +1,51 @@
+﻿using ApplicationLayer.Common.Exceptions;
+using ApplicationLayer.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApplicationLayer.Task.Commands.DeleteTask
+{
+	public class DeleteTaskCommand : IRequest<DomainLayer.Entities.Task>
+	{
+		public string Id { get; set; }
+
+		public DomainLayer.Entities.Task ToEntity() => new DomainLayer.Entities.Task {
+			Id = Id
+		};
+	}
+	public class DeleteTaskCommandHandler : IRequestHandler<DeleteTaskCommand, DomainLayer.Entities.Task>
+	{
+		private IApplicationDbContext AppDbContext { get; }
+		private ILogger Logger { get; }
+
+		public DeleteTaskCommandHandler(IApplicationDbContext context,
+			ILogger logger)
+		{
+			AppDbContext = context;
+			Logger = logger;
+		}
+		public async Task<DomainLayer.Entities.Task> Handle(DeleteTaskCommand request, CancellationToken cancellationToken)
+		{
+			try
+			{
+				var task = await AppDbContext.Tasks.FindAsync(request.ToEntity().Id);
+
+				if (task == null)
+					throw new NotFoundException();
+
+				AppDbContext.Tasks.Remove(task);
+				await AppDbContext.SaveChangesAsync(cancellationToken);
+
+				return task;
+			}
+			catch (Exception e)
+			{
+				await Logger.ErrorAsync(e);
+				throw;
+			}
+		}
+	}
+}
