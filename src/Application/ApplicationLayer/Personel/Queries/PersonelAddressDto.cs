6b72836a37ee30500d89bea53192e9146377a8e4
Yashar Aliabbasi <yashar@pragmateam.dev>
2021-04-01 20:16:46 +0430

    Implemented PersonelAddressDto

--- src/Application/ApplicationLayer/Personel/Queries/PersonelAddressDto.cs
+++ src/Application/ApplicationLayer/Personel/Queries/PersonelAddressDto.cs
@@ -1,6 +1,12 @@
-﻿namespace ApplicationLayer.Personel.Queries
+﻿using ApplicationLayer.Common.Mappings;
+
+namespace ApplicationLayer.Personel.Queries
 {
-	public class PersonelAddressDto
+	public class PersonelAddressDto : IMapFrom<DomainLayer.Entities.PersonelAddress>
 	{
+		public string Country { get; set; }
+		public string City { get; set; }
+		public string Street { get; set; }
+		public string No { get; set; }
 	}
 }
