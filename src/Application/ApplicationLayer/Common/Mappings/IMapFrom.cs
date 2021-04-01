3514ad9cd387184234c9a50911d9e00bd96291f2
Yashar Aliabbasi <yashar@pragmateam.dev>
2021-04-01 19:34:34 +0430

    Added Mapping base

---
+++ src/Application/ApplicationLayer/Common/Mappings/IMapFrom.cs
@@ -0,0 +1,9 @@
+﻿using AutoMapper;
+
+namespace ApplicationLayer.Common.Mappings
+{
+	public interface IMapFrom<T>
+    {
+        void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+    }
+}
