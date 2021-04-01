439159b5f29f41f1fc90883b030e235df5d744ae
Yashar Aliabbasi <yashar@pragmateam.dev>
2021-04-01 20:15:41 +0430

    Implemenetd PersonelTaskDto

--- src/Application/ApplicationLayer/Personel/Queries/PersonelTaskDto.cs
+++ src/Application/ApplicationLayer/Personel/Queries/PersonelTaskDto.cs
@@ -1,6 +1,23 @@
-﻿namespace ApplicationLayer.Personel.Queries
+﻿using ApplicationLayer.Common.Mappings;
+using AutoMapper;
+using System;
+
+namespace ApplicationLayer.Personel.Queries
 {
-	public class PersonelTaskDto
+	public class PersonelTaskDto : IMapFrom<DomainLayer.Entities.PersonelTask>
 	{
+		public string Title { get; set; }
+		public string Description { get; set; }
+		public DateTime? DueDate { get; set; }
+		public string Status { get; set; }
+
+		public void Mapping(Profile profile)
+		{
+			profile.CreateMap<DomainLayer.Entities.PersonelTask, PersonelTaskDto>()
+				.ForMember(d => d.Title, opt => opt.MapFrom(s => s.Task.Title))
+				.ForMember(d => d.Description, opt => opt.MapFrom(s => s.Task.Description))
+				.ForMember(d => d.DueDate, opt => opt.MapFrom(s => s.Task.DueDate))
+				.ForMember(d => d.Status, opt => opt.MapFrom(s => s.Task.Status.ToString()));
+		}
 	}
 }
